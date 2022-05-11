e97c98499ec76a35722db42769a93f973c85e575
Kost Kikinov <kikinov.k@gmail.com>
2022-05-11 15:33:22 +0300

    refactor: jQuery => JS and move to ./component

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import { GLOBAL_VARS } from 'utils/constants';
 import { documentReady, pageLoad } from 'utils';
 import pageWidgetInit from './dev_vendors/dev_widget';
+import mobilMenu from './components/mobil_menu';
 // ------------------- imports###
 
 // ------------------  import components
@@ -28,26 +29,7 @@ if (GLOBAL_VARS.projectDevStatus) {
 
 const readyFunc = () => {
 	console.log('ready');
-	const $menuTrigger = $('.menuTrigger');
-	const $menu = $('.headerPanel');
-	const $overlay = $('.overlay');
-	const $body = $('body');
-
-	function hendlerTrigger() {
-		if ($menu.hasClass('menu_open')) {
-			$menu.removeClass('menu_open');
-			$(this).removeClass('active_mod');
-			$body.removeClass('scroll-off');
-			$overlay.removeClass('visible');
-		} else {
-			$menu.addClass('menu_open');
-			$(this).addClass('active_mod');
-			$body.addClass('scroll-off');
-			$overlay.addClass('visible');
-		}
-	}
-
-	$menuTrigger.on('click', hendlerTrigger);
+	mobilMenu();
 };
 
 const loadFunc = () => {
---
+++ src/js/components/mobil_menu.js
@@ -0,0 +1,22 @@
+export default function mobilMenu() {
+	const $menuTrigger = document.querySelector('.menuTrigger');
+	const $menu = document.querySelector('.headerPanel');
+	const $overlay = document.querySelector('.overlay');
+	const $body = document.querySelector('body');
+
+	function hendlerTrigger() {
+		if ($menu.classList.contains('menu_open')) {
+			$menu.classList.remove('menu_open');
+			$menuTrigger.classList.remove('active_mod');
+			$body.classList.remove('scroll-off');
+			$overlay.classList.remove('visible');
+		} else {
+			$menu.classList.add('menu_open');
+			$menuTrigger.classList.add('active_mod');
+			$body.classList.add('scroll-off');
+			$overlay.classList.add('visible');
+		}
+	}
+
+	$menuTrigger.addEventListener('click', hendlerTrigger);
+}
