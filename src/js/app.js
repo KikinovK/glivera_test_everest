14f10b9c156476aa6aebfadf48cfc2e690ff9e5c
Kost Kikinov <kikinov.k@gmail.com>
2022-05-17 12:09:11 +0300

    rfactor: rename file of component and change function declaration to function expression

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import { GLOBAL_VARS } from 'utils/constants';
 import { documentReady, pageLoad } from 'utils';
 import pageWidgetInit from './dev_vendors/dev_widget';
-import mobilMenu from './components/mobil_menu';
+import mobilMenu from './components/mobilMenu';
 // ------------------- imports###
 
 // ------------------  import components
--- src/js/components/mobilMenu.js
+++ src/js/components/mobilMenu.js
@@ -1,10 +1,10 @@
-export default function mobilMenu() {
+const mobilMenu = () => {
 	const $menuTrigger = document.querySelector('.menuTrigger');
 	const $menu = document.querySelector('.headerPanel');
 	const $overlay = document.querySelector('.overlay');
 	const $body = document.querySelector('body');
 
-	function hendlerTrigger() {
+	const hendlerTrigger = () => {
 		if ($menu.classList.contains('menu_open')) {
 			$menu.classList.remove('menu_open');
 			$menuTrigger.classList.remove('active_mod');
@@ -16,7 +16,9 @@ export default function mobilMenu() {
 			$body.classList.add('scroll-off');
 			$overlay.classList.add('visible');
 		}
-	}
+	};
 
 	$menuTrigger.addEventListener('click', hendlerTrigger);
-}
+};
+
+export default mobilMenu;
